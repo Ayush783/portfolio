2edba19277f05cfdd18a9e47f23dd1578fa97683
Ayush783 <ayushc45xxx@gmail.com>
2022-07-17 09:02:48 +0530

    added homeview

--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
-import '../widgets/retro_background.dart';
+import '../widgets/background.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: RetroBackground(),
+      body: Stack(
+        children: const [
+          Background(),
+        ],
+      ),
     ));
   }
 }
