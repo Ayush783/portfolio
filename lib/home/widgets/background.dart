34e3637a519c002cd823e79364d3b0325cff24d6
Ayush783 <ayushc45xxx@gmail.com>
2022-07-17 19:21:17 +0530

    changed name of file

---
+++ lib/home/widgets/background.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/home/widgets/animated_component.dart';
+
+class Background extends StatelessWidget {
+  const Background({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: size.height,
+      decoration: const BoxDecoration(
+        color: Color(0xfff1f1f1),
+        gradient: RadialGradient(
+          colors: [
+            Color(0xfff1f1f1),
+            Color(0xffebebeb),
+          ],
+        ),
+      ),
+      child: Stack(
+        children: [
+          // Positioned(
+          //   bottom: 0,
+          //   left: 0,
+          //   child: AnimatedComponent.ring(),
+          // )
+        ],
+      ),
+    );
+  }
+}
