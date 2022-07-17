2b156bdacfb5f0743305cd796d6a8bee6adb2d7f
Ayush783 <ayushc45xxx@gmail.com>
2022-07-17 19:21:41 +0530

    added a theme directory

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class AppColors {
+  static const primaryColor = Color(0xff272727);
+
+  static const secondaryColor = Color(0xff8e8e8e);
+}
---
+++ lib/theme/typography.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+
+class AppTypography {
+  static const String family = 'roboto';
+  static TextStyle bodyTextstyle = const TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle boldBodyTextstyle = const TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle lightBodyTextstyle = const TextStyle(
+    fontFamily: family,
+    fontSize: 16,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle bodyTextstyle2 = const TextStyle(
+    fontFamily: family,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle boldBodyTextstyle2 = const TextStyle(
+    fontFamily: family,
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle lightBodyTextstyle2 = const TextStyle(
+    fontFamily: family,
+    fontSize: 20,
+    fontWeight: FontWeight.w300,
+  );
+
+  static TextStyle headingTextstyle = const TextStyle(
+    fontFamily: family,
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle boldHeadingTextstyle = const TextStyle(
+    fontFamily: family,
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle headingTextstyle2 = const TextStyle(
+    fontFamily: family,
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle boldHeadingTextstyle2 = const TextStyle(
+    fontFamily: family,
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+  );
+}
