8d7a804102382fe42a3ee10bf6126c3abbf96c8f
Ayush783 <ayushc45xxx@gmail.com>
2022-07-20 01:30:45 +0530

    pre chached an image

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/home/home.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    precacheImage(const AssetImage('assets/images/12.png'), context);
+  }
 
   @override
   Widget build(BuildContext context) {
