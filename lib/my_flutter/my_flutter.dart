0a9331c652acdfd6b10bbf957ef7722cf6692fe5
Ayush783 <ayushc45xxx@gmail.com>
2022-07-30 16:11:03 +0530

    added my flutter view

---
+++ lib/my_flutter/my_flutter.dart
@@ -0,0 +1,1 @@
+export 'view/my_flutter_view.dart';
---
+++ lib/my_flutter/view/my_flutter_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyFlutterView extends StatelessWidget {
+  const MyFlutterView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
