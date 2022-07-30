285ae2020259c43bc2c02d343d52b210f161aa5b
Ayush783 <ayushc45xxx@gmail.com>
2022-07-30 16:10:51 +0530

    added interests feature

---
+++ lib/interestes/interests.dart
@@ -0,0 +1,1 @@
+export 'view/interests_view.dart';
---
+++ lib/interestes/view/interests_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InterestsView extends StatelessWidget {
+  const InterestsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
