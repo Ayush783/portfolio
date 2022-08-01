d8bc15890d42cf6c2f76748d7fadf83a67357821
Ayush783 <ayushc45xxx@gmail.com>
2022-08-01 20:04:20 +0530

    added a precache all image function

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -12,7 +12,23 @@ class _AppState extends State<App> {
   @override
   void initState() {
     super.initState();
+    precacheAllImages(context);
+  }
+
+  precacheAllImages(BuildContext context) {
     precacheImage(const AssetImage('assets/images/12.png'), context);
+    precacheImage(const AssetImage('assets/images/1.png'), context);
+    precacheImage(const AssetImage('assets/images/2.png'), context);
+    precacheImage(const AssetImage('assets/images/3.png'), context);
+    precacheImage(const AssetImage('assets/images/4.png'), context);
+    precacheImage(const AssetImage('assets/images/5.png'), context);
+    precacheImage(const AssetImage('assets/images/6.png'), context);
+    precacheImage(const AssetImage('assets/images/country.png'), context);
+    precacheImage(const AssetImage('assets/images/db_cover.png'), context);
+    precacheImage(const AssetImage('assets/images/dsc.png'), context);
+    precacheImage(
+        const AssetImage('assets/images/flutter_translator.png'), context);
+    precacheImage(const AssetImage('assets/images/yodha.png'), context);
   }
 
   @override
