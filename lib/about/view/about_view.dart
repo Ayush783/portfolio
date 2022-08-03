fed90054074cd449f543d73a15b5db2329734798
Ayush783 <ayushc45xxx@gmail.com>
2022-08-03 17:13:49 +0530

    about view made responsive

--- lib/about/view/about_view.dart
+++ lib/about/view/about_view.dart
@@ -14,37 +14,59 @@ class AboutView extends ConsumerWidget {
     final h = MediaQuery.of(context).size.height;
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: w / 8, vertical: 20),
-      child: Row(
+      child: Stack(
+        alignment: Alignment.center,
         children: [
-          Expanded(
-            flex: 2,
-            child: Image.asset(
-              'assets/images/12.png',
-            ),
-          ),
-          SizedBox(width: 32.getResponsiveWidth(context)),
-          Expanded(
-            flex: 3,
-            child: SizedBox(
-              height: h / 2,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      text: 'Hello',
-                      style: AppTypography.boldHeadingTextstyle.copyWith(
-                        fontSize: 64,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 16),
-                  const Expanded(child: AnimatedTextSwitcher()),
-                ],
+          if (w <= 720)
+            Positioned(
+              right: w > 650
+                  ? -32
+                  : w > 500
+                      ? -72
+                      : -120,
+              child: Opacity(
+                opacity: 0.45,
+                child: Image.asset(
+                  'assets/images/12.png',
+                  height: h / 1.5,
+                  cacheHeight: h ~/ 1.5 + 1,
+                ),
               ),
             ),
-          )
+          Row(
+            children: [
+              if (w > 720)
+                Expanded(
+                  flex: 2,
+                  child: Image.asset(
+                    'assets/images/12.png',
+                  ),
+                ),
+              SizedBox(width: 32.getResponsiveWidth(context)),
+              Expanded(
+                flex: 3,
+                child: SizedBox(
+                  height: h / 2,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: 'Hello',
+                          style: AppTypography.boldHeadingTextstyle.copyWith(
+                            fontSize: w > 500 ? 64 : 48,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      const Expanded(child: AnimatedTextSwitcher()),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
