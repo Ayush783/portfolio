6347bd22439e86322497bb8b8c1fd75269e3f20a
Ayush783 <ayushc45xxx@gmail.com>
2022-08-04 22:43:26 +0530

    new build done

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -68,8 +68,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "2c7319229e59e4e671a1dc44da12f995",
-"/": "2c7319229e59e4e671a1dc44da12f995",
+"index.html": "f21dd5025669e40a7e353e1175e97c01",
+"/": "f21dd5025669e40a7e353e1175e97c01",
 "main.dart.js": "86b40cb7dd3b8b2f39c16ae2c9169629",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
